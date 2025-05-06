f51ce1319f5cfb312db595de4a2108130e7295f1
nviic <nviic@hotmail.com>
2025-05-06 14:00:48 +0000

    Deploying to gh-pages from @ nviic/nviic.github.io@f246515632e356e1ce25d59aa79f54697181a6aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,8 +44,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-教程",
+          title: "教程",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
