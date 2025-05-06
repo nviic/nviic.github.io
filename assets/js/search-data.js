bcb6aa8fc4e813fe270cdf35d520a01bec6641b9
nviic <nviic@hotmail.com>
2025-05-06 14:11:44 +0000

    Deploying to gh-pages from @ nviic/nviic.github.io@f0fa56df07dcbe9a83f302242de9585e7c6d4d1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-存储库",
+          title: "存储库",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
