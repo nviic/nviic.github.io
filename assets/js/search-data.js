d814af63892b54803c6731e352ffca9a7b39d4c6
nviic <nviic@hotmail.com>
2025-05-06 14:07:59 +0000

    Deploying to gh-pages from @ nviic/nviic.github.io@4d68712d53b16344d105ebf7516b1327a11f37ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,8 +51,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-人员",
+          title: "人员",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
